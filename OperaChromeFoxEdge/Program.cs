bce1ca5ab6b571f4e1cb42a5d159289520187ab5
Tyler Critchfield <Tyler.Critchfield@vu.com>
2019-05-03 14:03:27 -0500

    Web Browser

---
+++ OperaChromeFoxEdge/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace OperaChromeFoxEdge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TCPClient client = new TCPClient();
+        }
+    }
+}
